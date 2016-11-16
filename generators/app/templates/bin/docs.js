8489bfe2f0f7aecc4cf7468db0d7ac4f0c876741
Gustavo Villalta <gvillalta@daitangroup.com>
2016-11-16 14:17:33 -0200

    Fix lint errors

--- generators/app/templates/bin/docs.js
+++ generators/app/templates/bin/docs.js
@@ -1,23 +1,23 @@
-const Promise   = require('bluebird');
-const YAML      = require('json2yaml');
-const fs        = require('fs');
-const getconfig = require('getconfig');
-const path      = require('path');
-const rp        = require('request-promise');
+const Promise = require('bluebird');
+const YAML    = require('json2yaml');
+const fs      = require('fs');
+const config  = require('getconfig');
+const path    = require('path');
+const rp      = require('request-promise');
 
 const projectRoot = path.resolve(path.dirname(__dirname));
 const apiPath     = path.resolve(projectRoot, 'lib', 'api.js');
 const api         = require(apiPath).instance();
 
-const host            = 'http://' + getconfig.api.host + ':' + getconfig.api.port + '/';
+const host            = 'http://' + config.api.host + ':' + config.api.port + '/';
 const swaggerFilePath = path.resolve(projectRoot, 'swagger.yaml');
-const version         = getconfig.api.version;
+const version         = config.api.version;
 const writeFile       = Promise.promisify(fs.writeFile);
 
 const options     = {
   uri: host + version + '/api-docs',
   headers: {
-    api_key: config.api.key
+    api_key: config.api.key // eslint-disable-line camelcase
   },
   json: false
 };
