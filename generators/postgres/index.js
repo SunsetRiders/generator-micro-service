9e9eb68952e1f3339e7eb44c09de934b1c4e1e49
Juliano M. Coimbra <juliano@jmcoimbra.eng.br>
2017-03-12 18:27:21 -0300

    Adding Postgres generator

---
+++ generators/postgres/index.js
@@ -0,0 +1,46 @@
+const fs        = require('fs');
+const chalk     = require('chalk');
+const Generator = require('yeoman-generator');
+const importTemplateFiles = require('../lib/import-template-files');
+const importTemplateFilesDefault  = importTemplateFiles((filename) => filename)((filename) => filename);
+
+module.exports = class extends Generator {
+
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  initializing() {
+    this.props = {};
+  }
+
+  write() {
+    importTemplateFilesDefault([
+      'lib/pg-schemes.js',
+      'config/postgres.js'
+    ]);
+    const errMessage = '\n' +
+      chalk.bgRed('Postgres environment variables could not be appended in .env.example file, ' +
+      'please do it yourself.');
+    // getting the environment variables from template file
+    fs.readFile(this.templatePath('env.postgres'), 'utf8', (err, fileContents) => {
+      if (err) {
+        this.log(err);
+        this.log(errMessage);
+        return;
+      }
+      // appending it to .env.example
+      fs.appendFile(this.destinationPath('.env.example'), fileContents, (err) => {
+        if (err) {
+          this.log(err);
+          this.log(errMessage);
+          return;
+        }
+      });
+    });
+  }
+
+  install() {
+    this.npmInstall(['knex'], {'save': true});
+  }
+};
