2ddf326cd3c7592d294b9bb2d3a25bdd89c08529
Bruno Agostini <bagostini@daitangroup.com>
2017-03-21 17:23:25 -0300

    Improve logs on generic-error-handler.js

--- generators/app/templates/lib/generic-error-handler.js
+++ generators/app/templates/lib/generic-error-handler.js
@@ -1,39 +1,49 @@
-const logger = require('./logger').defaultLogger;
+const logger = require('./logger').default;
 
 /**
  * Every error throw by any route method on the API will be parsed
  * by this middlware function
+ *
  * @param {Error} err when we receive an exception
  * @param {Request} req The ExpressJS request object
  * @param {Response} res The ExpressJS response object
  * @param {Next} next The ExpressJS next object
+ *
+ * @return {null} nothing important
  */
 module.exports = function(err, req, res, next) {
   let status = err.status || 500;
 
-  logger.info('genericErrorHandler', JSON.stringify({
-    request: {
-      url: req.url,
-      headers: req.headers,
-      method: req.method,
-      httpVersion: req.httpVersion,
-      originalUrl: req.originalUrl,
-      query: req.query,
-      params: req.params,
-      body: req.body
-    },
-    error: {
-      message: err.message || JSON.stringify(err)
-    }
-  }));
+  let printData = 'genericErrorHandler ... \n';
+
+  printData += '\n --- Request --- \n';
+  printData += JSON.stringify({
+    httpVersion: req.httpVersion,
+    method:      req.method,
+    url:         req.url,
+    originalUrl: req.originalUrl,
+    headers:     req.headers,
+    query:       req.query,
+    params:      req.params,
+    body:        req.body
+  }, null, 2);
+
+  printData += '\n --- Error --- \n';
+  printData += JSON.stringify(err, null, 2) + '\n';
+
+  logger.debug(printData);
 
   if (err.errors) {
-    res.status(status).json({errors: err.errors});
-    return;
+    return res.status(status).json({
+      errors: err.errors
+    });
   }
 
-  res.status(status).json(
-    {errors: [
-      {message: err.message}]}
-  );
+  return res.status(status).json({
+    errors: [
+      {
+        message: err.message
+      }
+    ]
+  });
 };
